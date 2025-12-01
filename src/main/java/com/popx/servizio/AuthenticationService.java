1f3227bd5d5a982e6dd16dbfc0ab051f7554dd29
Daniele Pio Scaparra <162312256+dscap02@users.noreply.github.com>
2025-12-01 15:56:39 +0100

    docs: Added JML specifications for Services classes Added JML specs for AuthenticationService class and for Security Service classes

--- src/main/java/com/popx/servizio/AuthenticationService.java
+++ src/main/java/com/popx/servizio/AuthenticationService.java
@@ -1,23 +1,40 @@
 package com.popx.servizio;
 
 import com.popx.modello.UserBean;
-import com.popx.persistenza.DataSourceSingleton;
 import com.popx.persistenza.UserDAO;
 import com.popx.persistenza.UserDAOImpl;
 
 import javax.sql.DataSource;
 
+/*@ public invariant userDAO != null; @*/
 public class AuthenticationService {
+
     private DataSource DataSourceSingleton;
     private UserDAO userDAO = new UserDAOImpl();
 
+    /*@ ensures this.userDAO != null; @*/
     public AuthenticationService(){
     }
 
+    /*@
+      @ requires userDAO != null;
+      @ ensures this.userDAO == userDAO;
+      @*/
     public AuthenticationService(UserDAO userDAO){
         this.userDAO = userDAO;
     }
 
+    /*@
+      @ public normal_behavior
+      @ requires email != null && !email.isEmpty();
+      @ requires password != null && !password.isEmpty();
+      @
+      @ ensures \result != null ==>
+      @      (\result.getEmail().equals(email));
+      @
+      @ signals (Exception e)
+      @      e.getMessage().equals("Invalid credentials");
+      @*/
     public UserBean login(String email, String password) throws Exception {
         UserBean user = userDAO.getUserByEmail(email);
 
@@ -27,7 +44,15 @@ public class AuthenticationService {
         throw new Exception("Invalid credentials");
     }
 
-
+    /*@
+      @ public normal_behavior
+      @ requires user != null;
+      @ requires user.getEmail() != null && !user.getEmail().isEmpty();
+      @
+      @ ensures \result == true || \result == false;
+      @ signals (Exception e)
+      @      e.getMessage().equals("User already exists");
+      @*/
     public boolean registerUser(UserBean user) throws Exception {
         if (userDAO.getUserByEmail(user.getEmail()) == null) {
             return userDAO.saveUser(user);
@@ -35,10 +60,23 @@ public class AuthenticationService {
         throw new Exception("User already exists");
     }
 
+    /*@
+      @ public normal_behavior
+      @ requires email != null && !email.isEmpty();
+      @ ensures \result == true || \result == false;
+      @ signals (Exception) true;
+      @*/
     public boolean isEmailRegistered(String email) throws Exception {
         return userDAO.getUserByEmail(email) != null;
     }
 
+    /*@
+      @ public normal_behavior
+      @ requires email != null && !email.isEmpty();
+      @ ensures \result != null;
+      @ ensures \result.equals( userDAO.getUserByEmail(email).getRole() );
+      @ signals (Exception) true;
+      @*/
     public String retrieveRole(String email) throws Exception {
         UserBean user = userDAO.getUserByEmail(email);
         return user.getRole();
--- src/main/java/com/popx/servizio/SecurityService.java
+++ src/main/java/com/popx/servizio/SecurityService.java
@@ -4,12 +4,31 @@ import org.mindrot.jbcrypt.BCrypt;
 
 public class SecurityService {
 
+    /*@
+      @ public normal_behavior
+      @ requires password != null && !password.isEmpty();
+      @
+      @ // L’hash è sempre non nullo e diverso dalla password in chiaro
+      @ ensures \result != null;
+      @ ensures !\result.equals(password);
+      @
+      @ // Un hash valido BCrypt inizia sempre con "$2"
+      @ ensures \result.startsWith("$2");
+      @*/
     public static String hashPassword(String password) {
         int costFactor = 12;
         String salt = BCrypt.gensalt(costFactor);
         return BCrypt.hashpw(password, salt);
     }
 
+    /*@
+      @ public normal_behavior
+      @ requires password != null && !password.isEmpty();
+      @ requires hashedPassword != null && !hashedPassword.isEmpty();
+      @
+      @ // L’output è sempre booleano
+      @ ensures \result == true || \result == false;
+      @*/
     public static boolean checkPassword(String password, String hashedPassword) {
         return BCrypt.checkpw(password, hashedPassword);
     }
